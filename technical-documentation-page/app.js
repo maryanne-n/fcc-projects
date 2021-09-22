fffd600ef6065afb15355be666d98ad7d56a2576
maryanne-n <mng8hc@gmail.com>
2021-09-22 23:02:16 +1000

    Initial upload of technical documentation page project

---
+++ technical-documentation-page/app.js
@@ -0,0 +1,50 @@
+// Jquery add active class
+// $(document).ready(function() {
+//   "use strict";
+
+//   $('.nav-item').click(function(e) {
+//     // e.preventDefault();
+//     $('.nav-item').removeClass('highlight');
+//     $(this).addClass('highlight');
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function() {     
+
+    // Javascript add highlight class to indicate current section
+    var navMenu = document.getElementById("navmenu");
+    var navItems = navMenu.getElementsByClassName("nav-item");
+
+    for (var i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("highlight");
+        current[0].className = current[0].className.replace(" highlight", "");
+        this.className += " highlight"
+    });
+    }
+
+    const hamburger = document.querySelector(".hamburger");
+    const navBar = document.querySelector("#navbar");
+    const header = navBar.querySelector("header");
+
+    hamburger.addEventListener("click", mobileMenu);
+
+    function mobileMenu() {
+    
+        hamburger.classList.toggle("active");
+        navBar.classList.toggle("active");
+        navMenu.classList.toggle("active");
+        header.classList.toggle("active");
+    }
+
+    const navLink = document.querySelectorAll(".nav-link");
+
+    navLink.forEach(navLink => navLink.addEventListener("click", closeMenu));
+
+    function closeMenu() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        navBar.classList.remove("active");
+        header.classList.remove("active");
+    }
+}
